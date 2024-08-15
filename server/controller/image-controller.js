04911a04b0604f0eb1328c561a1c50cd8e2af9b0
shallypagariya <shallypagariya@gmail.com>
2024-08-15 19:53:27 +0530

    Update image-controller.js

--- server/controller/image-controller.js
+++ server/controller/image-controller.js
@@ -1,39 +1,4 @@
-// import grid from 'gridfs-stream';
 
-// import mongoose from 'mongoose';
-
-// const url='http://localhost:8080'
-// let gfs,gridfsBucket;
-// const conn=mongoose.connection;
-// conn.once('open',()=>{
-//     gridfsBucket=new mongoose.mongo.GridFSBucket(conn.db,{
-//         bucketName :'fs'
-//     });
-//     gfs=grid(conn.db,mongoose.mongo);
-//     gfs.collection('fs');
-// })
-
-// export const uploadImage=(request,response)=>{
-//     if(!request.file){
-//         return response.status(404).json({msg :"File Not found "});
-//     }
-//     const imageUrl=`${url}/file/${request.file.filename}`;
-//     return response.status(200).json(imageUrl);
-// }
-
-// export const getImage=async(response,request)=>{
-
-//     try{
-//         const file=await gfs.files.findOne({filename :request.params.filename});
-//         const readStream=gridfsBucket.openDownloadStream(file._id);
-//         readStream.pipe(response);
-//     }catch(error){
-//         return response.status(500).json({msg :error.message});
-//     }
-
-
-
-// }
 
 import grid from 'gridfs-stream';
 import mongoose from 'mongoose';
@@ -41,7 +6,7 @@ import mongoose from 'mongoose';
 const url = 'http://localhost:8080';
 let gfs, gridfsBucket;
 
-// Assuming mongoose is properly configured and connected
+
 const conn = mongoose.connection;
 
 conn.once('open', () => {
@@ -50,7 +15,7 @@ conn.once('open', () => {
     bucketName: 'fs'
   });
   gfs = grid(conn.db, mongoose.mongo);
-  gfs.collection('fs'); // Specify the collection name (optional)
+  gfs.collection('fs'); 
 });
 
 export const uploadImage = (req, res) => {
